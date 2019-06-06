cacf337d87592d6ff49e8869ebd14756b457f05f
Harsger <herrmannmaximilian@web.de>
2019-06-06 12:23:50 +0200

    python script added for iteration of jobs over similar files

---
+++ scripter.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+
+import sys, getopt
+import os
+#import ROOT
+import time
+import math
+import csv
+import time
+from array import array
+import subprocess
+
+infiles = {}
+#count = 0
+
+def fillInputFiles (dirPath,inputfile,specific,exclude):
+       
+    InDir=os.listdir(dirPath)
+    for filename in InDir:
+        if filename.startswith(str(inputfile)) and filename.find(str(specific)) != -1 and filename.find(str(exclude)) == -1:
+            infiles[filename] = filename 
+            #count += 1
+            print " "+str(filename)
+
+def main(argv):
+    #global count
+
+    commandStart = ''
+    endCommand = ''
+    inputfile = ''
+    datapath = ''
+    specifier = ''
+    notuse = 'neverUseThisPhrase'
+    
+    filename = ''
+    usage = 'python scripter.py -c \'<commandStart>\' -e \'<endCommand>\' -i <inputfile> -d <datapath> -s <specifier> -n <notuse>'
+    
+    try:
+        opts, args = getopt.getopt(argv,"c:e:i:d:s:n:",["commandStart=","endCommand","inputfile=","datapath=","specifier=","notuse="])
+    except getopt.GetoptError:
+        print usage
+        sys.exit(2)
+            
+    if len(argv) < 1:
+        print " arguments required "
+        print str(usage)
+        sys.exit(2)
+            
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print usage
+            sys.exit()
+        elif opt in ("-c", "--commandStart"):
+            commandStart = arg
+        elif opt in ("-e", "--endCommand"):
+            endCommand = arg
+        elif opt in ("-i", "--inputfile"):
+            inputfile = arg
+        elif opt in ("-d", "--datapath"):
+            datapath = arg
+        elif opt in ("-s", "--specifier"):
+            specifier = arg
+        elif opt in ("-n", "--notuse"):
+            notuse = arg
+                
+    if inputfile.find('/') != -1:
+        charsINstr = len(inputfile)
+        indexLastSlash = inputfile.rfind( '/' , 0 , charsINstr )
+        datapath = inputfile[0:indexLastSlash]
+        inputfile = inputfile[indexLastSlash+1:charsINstr]
+                    
+    print " commandStart    : "+str(commandStart)
+    print " endCommand      : "+str(endCommand)
+    print " inputfiles      : "+str(inputfile)
+    print " datapath        : "+str(datapath)
+    print " specifier       : "+str(specifier)
+    print " notuse          : "+str(notuse)
+            
+    if inputfile == '' or commandStart == '':
+        print usage
+        sys.exit(2)
+
+    fillInputFiles( datapath, inputfile, specifier, notuse )
+
+    numberOfFiles = len(infiles)
+    
+    print " # found files : "+str(numberOfFiles)
+    
+    count = 0
+    
+    for filename in infiles:
+        
+        readname = str(datapath)+"/"+str(filename)
+        fullcommand = str(commandStart) + str(readname) + str(endCommand)
+        
+        count += 1
+        print " job "+str(count)+" / "+str(numberOfFiles)+" : "+str(fullcommand)
+        
+        os.system(fullcommand)
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
