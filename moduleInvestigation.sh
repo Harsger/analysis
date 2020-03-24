961bf1b63f725912d6d9bdc7d38a6e6088ba6132
Harsger <herrmannmaximilian@web.de>
2020-03-24 10:04:31 +0100

    added option to skip whole analysis and skip certain unixtimes

--- moduleInvestigation.sh
+++ moduleInvestigation.sh
@@ -9,18 +9,33 @@ filename=$2
 endPhrase=".root"
 filename=${filename%$endPhrase}
 
+timeskip=$3
+
 analysisPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/"
 dataPath="/project/etpdaq4/CRF_data/nswQAQC/m"${moduleNumber}"/"
 
 fillHistograms=${analysisPath}"investigateCRF -i "${filename}".root -p "${dataPath}"parameter/parameter_SM2.txt -d "${dataPath}"fitteddata -o "${dataPath}"histograms"
 postprocess=${analysisPath}"postprocessor -i "${filename}"_inCRF.root -p "${dataPath}"parameter/parameter_SM2.txt -d "${dataPath}"histograms -m "
 
+if [ "${timeskip}" != "" ]; then    
+    fillHistograms+=" -u "${timeskip}
+fi
+
 echo ${fillHistograms}
 ${fillHistograms}
-echo ${postprocess}"properties"
-${postprocess}"properties"
-echo ${postprocess}"align"
-${postprocess}"align"
-echo ${postprocess}"precision"
-${postprocess}"precision"
+
+
+if [ $# -eq 2 ]
+then
+    echo ${postprocess}"properties"
+    ${postprocess}"properties"
+    echo ${postprocess}"align"
+    ${postprocess}"align"
+    echo ${postprocess}"precision"
+    ${postprocess}"precision"
+else
+    postprocess+="properties -r 50"
+    echo ${postprocess}
+    ${postprocess}
+fi
 
